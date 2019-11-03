0fdadf77680fda03a6262fd2d82a8526eb8d9dd5
Rafael Fernández <rafaelfernandezortiz@gmail.com>
2019-11-03 21:06:33 +0100

    init
    init

--- mail.py
+++ mail.py
@@ -1,29 +1,5 @@
-# Gmail Attachment
-
-El siguiente proyecto consiste en la descarga de los archivos adjuntos a los mails que se ha recibido en el mes actual. El cliente de correos que se utilzia es el de Google, esto es, Gmail.
-
-Los datos de cuenta de correo y contraseña, de momento, iran en texto plano en un archivo yml de configuración. No es lo apropiado, puesto que debería ir securizado, sin embargo se estudiará ese punto en un futuro.
-
-Una alternativa a esa forma de conexión seria mediante un prompt a la shell que pidiera tanto email como contraseña, lo cual sí sería más "seguro", pero una tarea incómoda tanto para un desarrollo como para el consumo de la aplicación de forma excesivamente reiterada.
-
-## Requisitos
-
-Antes de continuar, se debe [habilitar los permisos de aplicaciones poco seguras](https://myaccount.google.com/lesssecureapps) en la cuenta de Gmail que vayamos a utilizar y habilitar el [Acceso via IMAP](https://support.google.com/mail/answer/7126229?hl=es).
-
-Puede obtener mayor información aqui: [Aplicaciones poco seguras y la cuenta de Google](https://support.google.com/accounts/answer/6010255?hl=es)
-
-## Código
-
-#### Archivo de configuración YML
-
-```yaml
-user: user@gmail.com # Cuenta de gmail
-passwd: passswd # Password
-```
-
-#### mail.py
-
-```python
+# Make sure you have IMAP enabled in your gmail settings.
+# Right now it won't download same file name twice even if their contents are different.
 import datetime, email, imaplib, os
 import yaml
 
@@ -81,12 +57,3 @@ class mail():
 if __name__ == "__main__":
     mail = mail("config_mail.yml")
     mail.latest_mails()
-
-```
-
-## Autores ✒️
-
-_Se agradece la reseña o cita del autor, de su trabajo y del propio repositorio en los trabajos a los que haya aportado algo de luz y conocimiento._
-
-**Rafael Fernández Ortiz**.- :briefcase: [LinkedIn](https://www.linkedin.com/in/rafael-fern%C3%A1ndez-ortiz-7a1684171/) - ​<img src="https://img.icons8.com/color/20/000000/open-envelope.png">​ ​[Gmail](mailto:rafaelfernandezortiz@gmail.com) - ​<img src="https://img.icons8.com/color/20/000000/cardboard-box.png">​ [GitHub](https://github.com/rafafrdz) - :bookmark_tabs: [Cv](https://rafafrdz.github.io/) 
-
